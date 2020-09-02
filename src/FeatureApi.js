1fa21ff9ec73eca25c178022d3b51873cf4b4d48
Zachary Maillard <zmaillard@healthwise.org>
2020-09-02 15:18:15 -0600

    Feedback

--- src/FeatureApi.js
+++ src/FeatureApi.js
@@ -5,6 +5,14 @@ function baseAzureTableApi(table, index='') {
   return `https://clinicalabdatadev.table.core.windows.net/${table}${index}${process.env.STORAGE_TOKEN}`;
 }
 
+// https://stackoverflow.com/questions/105034/how-to-create-guid-uuid
+function uuidv4() {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
+}
+
+
 function authHeaders() {
   return {
       'Content-Type': 'application/json',
@@ -38,4 +46,17 @@ export function updateFeatureFlag(flagName, newValue) {
         },
         headers: authHeaders(),
     });
+}
+
+export function addFeedback(feedback, featureType) {
+  var url = baseAzureTableApi("feedback");
+  var data = {
+    feedback,
+    featureType,
+    feedbackDate: new Date().getUTCDate(),
+    PartitionKey: "feedback",
+    RowKey: uuidv4(),
+  };
+
+  return axios.post(url, data, {headers: authHeaders()}); 
 }
